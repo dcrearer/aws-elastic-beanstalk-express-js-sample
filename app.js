8b0ea2bdab7c29a9a1d2f65394b1f22e10a5ac9e
dcrearer <dcrearer@gmail.com>
2021-01-26 22:59:37 -0500

    change console message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('crearerd-made changes'));
+app.get('/', (req, res) => res.send('crearerd-new commit'));
 
 
 app.listen(port);
